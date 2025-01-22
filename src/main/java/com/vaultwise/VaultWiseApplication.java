32b9f74afee821a957c7a7e2bbae38f48570c9ba
Gzim Jusufi <gj31060@seeu.edu.mk>
2025-01-22 18:12:04 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/vaultwise/VaultWiseApplication.java
@@ -0,0 +1,13 @@
+package com.vaultwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultWiseApplication.class, args);
+	}
+
+}
